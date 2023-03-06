53f57bb606b2c614146959c502c27756e1e8218c
Robert <rlgallegos85@gmail.com>
2023-03-06 11:13:55 -0500

    undid changes so I can make a separate branch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
@@ -8,9 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
