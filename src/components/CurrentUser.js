5ffd363b986a149cea3e127b8a402fed8b97503a
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-03-08 17:02:06 -0500

    CurrentUser buttons working

--- src/components/CurrentUser.js
+++ src/components/CurrentUser.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function CurrentUser({ currentUser }) {
   const { username, email, state } = currentUser;
+
   console.log(username, email, state);
   console.log(state);
   //   const score = state.score;
@@ -10,13 +11,31 @@ function CurrentUser({ currentUser }) {
       <h2 className="CurrentUserTitle">Current Player: {username}</h2>
       <table className="CurrentLoginMenu">
         <thead>
-          <tr>{currentUser ? <td>Score:{state["score"]}</td> : <td>error</td>}</tr>
+          <tr>
+            {currentUser.state["score"] ? (
+              <td>Score:{state["score"]}</td>
+            ) : (
+              <td>Score: 0</td>
+            )}
+          </tr>
           <tr>
             <td>Email: {email}</td>
           </tr>
-          <tr>{currentUser ? <td>Current Level:{state["level"]}</td> : null}</tr>
           <tr>
-            <td>Lives Remaining: {state["livesRemaining"]}</td>
+            {currentUser.state["level"] ? (
+              <td>Current Level:{state["level"]}</td>
+            ) : (
+              <td>Current Level: 1</td>
+            )}
+          </tr>
+          <tr>
+            <td>
+              {currentUser.state["livesRemaining"] ? (
+                <td>Lives Remaining: {state["livesRemaining"]}</td>
+              ) : (
+                <td>Lives Remaining: 3</td>
+              )}
+            </td>
           </tr>
         </thead>
       </table>
