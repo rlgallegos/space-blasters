c258d8f4f65fdd4a64986792ea52c3f34a463803
Robert <rlgallegos85@gmail.com>
2023-05-17 16:13:35 -0400

    Try 2

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const serverPort = 9000;
-const appPort = 8000;
+const port = process.env.PORT || 3000;
 
-// app.use(express.static('public'));
-// app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -19,14 +16,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 server.use(middlewares);
 server.use(router);
 
-server.listen(serverPort, () => {
-  console.log(`JSON Server is running on port ${serverPort}`);
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(appPort, () => {
-  console.log(`Static files server is running on port ${appPort}`);
-});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
