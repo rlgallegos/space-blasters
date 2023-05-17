cc1aec863c19618f4b63238b5f9b7c778693817f
Robert <rlgallegos85@gmail.com>
2023-05-17 19:26:57 -0400

    Finished server.js setup and testing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
