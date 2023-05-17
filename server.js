160f1d645d6d37fd540f47863aedb2c514ef7c17
Robert <rlgallegos85@gmail.com>
2023-05-17 00:23:38 -0400

    try on trillion

--- server.js
+++ server.js
@@ -7,22 +7,23 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const backendPort = 9000;
-const frontendPort = 8000;
+const serverPort = 9000;
+const appPort = 8000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(backendPort, () => {
-  console.log(`JSON Server is running on port ${backendPort}`);
+server.listen(serverPort, () => {
+  console.log(`JSON Server is running on port ${serverPort}`);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(frontendPort, () => {
-  console.log(`Static files server is running on port ${frontendPort}`);
+app.listen(appPort, () => {
+  console.log(`Static files server is running on port ${appPort}`);
 });
