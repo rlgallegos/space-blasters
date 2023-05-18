6e9c1436afe58dbc4477f2c70afe79c928436878
Robert <rlgallegos85@gmail.com>
2023-05-17 23:42:59 -0400

    trying to get backend to work now

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ function App() {
   // music.load();
 
   useEffect(() => {
-    fetch("http://localhost:3000/users")
+    fetch("/users")
       .then((res) => res.json())
       .then((data) => {
         console.log('GET from main users route')
