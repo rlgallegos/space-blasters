7fe4fada05679a1cc2306f4d253b5289cb514fc2
Robert <rlgallegos85@gmail.com>
2023-05-17 23:25:08 -0400

    Created frontend route

--- server.js
+++ server.js
@@ -28,17 +28,23 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const buildPath = path.join(__dirname, 'build')
+console.log(buildPath)
 
 server.use(middlewares);
 server.use(router);
 
 const port = 3000;
+const appPort = 8000;
 
 app.use(express.static(path.join(buildPath)));
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'))
+})
+
+app.listen(appPort, () => {
+  console.log(`Frontend Server is running on port ${appPort}`);
 })
 
 server.listen(port, () => {
