d3851fe4302a84327efd1bf61fc531c03dab19f8
Robert <rlgallegos85@gmail.com>
2023-05-17 20:00:44 -0400

    Updated server.js to include static folder serving

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ server.use(router);
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
