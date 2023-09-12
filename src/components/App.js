621ae28d54bd3786c8be52e0425d0de64aea4686
Robert <rlgallegos85@gmail.com>
2023-09-12 01:01:04 -0400

    Updating routes

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import IDMenu from "./IDMenu";
 
 const music = new Audio("./menuMusic.mp3");
 
-const BACKEND_URL = process.env.API_URL
+const BACKEND_URL = process.env.REACT_APP_API_URL;
 
 
 function App() {
